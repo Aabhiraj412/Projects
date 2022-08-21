6963d1b447c8ed2d872c3213deff59961b75e212
Abhiraj <aabhiraj412@gmail.com>
2022-08-21 14:46:10 +0530

    Done Here

---
+++ area.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a;
+
+    printf("Please Enter Length of Side Of Square -> ");
+
+    scanf("%d", &a);
+
+    printf("Area Of Square Is -> %d\n", a*a);
+    
+    return 0;
+}
